ee089f1f8095b2830c460005c6c9b2ff526da33c
IFOV37 <kkoster37@gmail.com>
2017-11-22 14:05:17 -0500

    starting getPokemon function

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,18 @@
 // pokemon page
+
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+    function getPokemon(res, mysql, context, complete){
+    	mysql.pool.query("SELECT id, name, type, attack, trainer.id AS Trainer FROM Pokemon INNER JOIN trainer ON trainer.id = pokemon.trainerID", function(error, results, fields){
+    		if(error){
+    			res.write(JSON.stringify(error));
+    			res.end();
+    		}
+    	})
+    }
+
+
+    return router;
+}();
