d91ca02884afb06094802b4d65df083715cb0e2e
IFOV37 <kos37ter96@hotmail.com>
2017-11-23 00:36:02 -0500

    update home js and HB files

--- home.js
+++ home.js
@@ -1,1 +1,25 @@
 // landing page js file, if required.
+
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+
+    router.get('/', function(req, res){
+        /*var callbackCount = 0;
+        var context = {};
+        //context.jsscripts = ["deletepokemon.js"]  // we need to pass our script/function if we want to actually be able to delete a pokemon  review if we want to later
+        var mysql = req.app.get('mysql');
+
+        getTrainers(res, mysql, context, complete);
+
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){ */
+                res.render('home', context);
+    /*        }
+        }*/
+    });
+
+    return router;
+}();
