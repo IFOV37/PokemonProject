f1ebe78a58b34bb45a3b181b255a707a78f142cd
IFOV37 <kos37ter96@hotmail.com>
2017-12-02 00:27:46 -0500

    adjusted queries per video

--- pokemon.js
+++ pokemon.js
@@ -19,7 +19,8 @@ module.exports = function(){
     // get single pokemon by id
     function getSinglePokemon(res, mysql, context, id, complete){
 
-        var sql = "SELECT p.id, p.name, p.type, p.attack, t.name AS 'Trainer' FROM Pokemon p LEFT JOIN Trainers t ON t.id = p.trainerID WHERE p.id = ?";
+        //var sql = "SELECT p.id, p.name, p.type, p.attack, t.name AS 'Trainer' FROM Pokemon p LEFT JOIN Trainers t ON t.id = p.trainerID WHERE p.id = ?";
+        var sql = "SELECT p.id, p.name, p.type, p.attack, p.trainerID FROM Pokemon p WHERE p.id = ?";
         var inserts = [id];
         mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
