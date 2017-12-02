cbc026954711c3ef07f4306f8295bc441c950e81
IFOV37 <kos37ter96@hotmail.com>
2017-12-02 17:23:18 -0500

    delete on add

--- public/deladd-badge.js
+++ public/deladd-badge.js
@@ -1,6 +1,6 @@
 function deleteBadge(bid, tid){
     $.ajax({
-        url: '/deleteBadge/' + bid + '/' + tid,
+        url: '/trainers/deleteBadge/' + bid + '/' + tid,
         type: 'DELETE',
         success: function(result){
             window.location.reload(true);
