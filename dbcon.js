445434a68ec90857b3a212c2385c4581a26e89b0
IFOV37 <kos37ter96@hotmail.com>
2017-12-03 02:15:38 -0500

    proper dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_berneran',
-  password        : 'frogger91[OSU]',
-  database        : 'cs340_berneran'
+  user            : 'cs340_un',
+  password        : '####',
+  database        : 'cs340_un'
 });
 module.exports.pool = pool;
