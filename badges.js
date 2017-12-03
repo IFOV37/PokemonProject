d8e56471c4c5984d843c07071ed2ec963fa17500
IFOV37 <kos37ter96@hotmail.com>
2017-12-02 22:28:10 -0500

    add search dd badges

--- badges.js
+++ badges.js
@@ -85,7 +85,7 @@ module.exports = function(){
 
     router.post('/search', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "SELECT name, color FROM Badges WHERE color LIKE %?%";
+        var sql = "SELECT name, color FROM Badges WHERE color = ?";
         var inserts = [req.body.color];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
